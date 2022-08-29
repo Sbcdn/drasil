3d0a970c53a9ed3b7063c0b1802f7609cfdf0945
Torben <81830852+Sbcdn@users.noreply.github.com>
2022-08-29 20:36:40 +0200

    Delete utxopti/.cargo directory
    Signed-off-by: Torben <81830852+Sbcdn@users.noreply.github.com>

--- utxopti/.cargo/gungnir/migrations/2022-04-03-135216_alter_enum_rwd_types/up.sql
+++
@@ -1,8 +0,0 @@
---#################################################################################
---# See LICENSE.md for full license information.                                  #
---# Software: Drasil Blockchain Application Framework                             #
---# License: Drasil Source Available License v1.0                                 #
---# Licensors: Torben Poguntke (torben@drasil.io) & Zak Bassey (zak@drasil.io)    #
---#################################################################################
-
-ALTER TYPE public.calculationmode ADD VALUE  IF NOT EXISTS 'custom' BEFORE 'fixedamountperepoch';
--- utxopti/.cargo/murin/src/txbuilders/stdtx/mod.rs
+++
@@ -1,9 +0,0 @@
-/*
-#################################################################################
-# See LICENSE.md for full license information.                                  #
-# Software: Drasil Blockchain Application Framework                             #
-# License: Drasil Source Available License v1.0                                 #
-# Licensors: Torben Poguntke (torben@drasil.io) & Zak Bassey (zak@drasil.io)    #
-#################################################################################
-*/
-pub mod build_cpo;
