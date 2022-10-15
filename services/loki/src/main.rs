d20b8006d3e3803cc09a3a8c97035c90f43ff544
Torben <torben@drasil.io>
2022-10-15 13:44:10 +0200

    loki

---
+++ services/loki/src/main.rs
@@ -0,0 +1,167 @@
+/*
+#################################################################################
+# See LICENSE.md for full license information.                                  #
+# Software: Drasil Blockchain Application Framework                             #
+# License: Drasil Source Available License v1.0                                 #
+# Licensors: Torben Poguntke (torben@drasil.io) & Zak Bassey (zak@drasil.io)    #
+#################################################################################
+*/
+extern crate pretty_env_logger;
+
+mod auth;
+mod error;
+mod filters;
+mod handlers;
+mod models;
+
+use deadpool_lapin::Pool;
+use lapin::ConnectionProperties;
+use models::Clients;
+use std::env;
+use std::{collections::HashMap, str, sync::Arc};
+use tokio::sync::Mutex;
+use warp::{Filter, Rejection};
+
+use nonzero_ext::nonzero;
+use ratelimit_meter::{DirectRateLimiter, LeakyBucket};
+
+const DEFAULT_HOST: &str = "127.0.0.1";
+const DEFAULT_PORT: &str = "40001";
+
+#[tokio::main]
+async fn main() -> Result<(), error::Error> {
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "info");
+    }
+
+    let host: String = env::var("POD_HOST").unwrap_or_else(|_| DEFAULT_HOST.to_string());
+    let port = env::var("POD_PORT").unwrap_or_else(|_| DEFAULT_PORT.to_string());
+
+    pretty_env_logger::init();
+
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["GET", "POST", "OPTIONS", "PUT"])
+        .allow_credentials(true)
+        .allow_headers(vec![
+            "Access-Control-Allow-Origin",
+            "Access-Control-Allow-Credentials",
+            "Access-Control-Allow-Headers",
+            "Access-Control-Allow-Methods",
+            "Access-Control-Expose-Headers",
+            "Access-Control-Max-Age",
+            "Access-Control-Request-Headers",
+            "Access-Control-Request-Method",
+            "Origin",
+            "XMLHttpRequest",
+            "X-Requested-With",
+            "Accept",
+            "Content-Type",
+            "Referer",
+            "User-Agent",
+            "sec-ch-ua",
+            "sec-ch-ua-mobile",
+            "sec-ch-ua-platform",
+            "Accept-Encoding",
+            "Accept-Language",
+            "authorization",
+            "Connection",
+            "Content-Length",
+            "Host",
+            "Sec-Fetch-Dest",
+            "Sec-Fetch-Mode",
+            "Sec-Fetch-Site",
+        ]);
+
+    // RMQ
+    let addr =
+        std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://rmq:rmq@127.0.0.1:5672/%2f".into());
+    let manager = deadpool_lapin::Manager::new(addr, ConnectionProperties::default());
+    let pool: deadpool_lapin::Pool = deadpool::managed::Pool::builder(manager)
+        .max_size(100)
+        .build()
+        .expect("can't create pool");
+    println!("pool: {:?}", pool);
+    let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
+
+    //Rate Limitation
+    // Allow 3 units/second across all threads:
+    let lim =
+        DirectRateLimiter::<LeakyBucket>::new(nonzero!(2u32), std::time::Duration::from_secs(5));
+    let api = filters::endpoints(clients.clone(), &pool, lim);
+
+    log::info!("Configuring websocket route");
+    let routes = api
+        .with(cors)
+        .recover(filters::handle_rejection)
+        .with(warp::log("loki"));
+    log::info!("Starting update loop");
+    tokio::task::spawn(async move {
+        handlers::main_worker(clients.clone()).await;
+    });
+    log::info!("Starting server");
+
+    let server = host.clone() + ":" + &port;
+    let socket: std::net::SocketAddr = server.parse().expect("Unable to parse socket address");
+
+    let _ = futures::join!(warp::serve(routes).run(socket)); // rmq_listen(pool)
+    Ok(())
+}
+
+async fn get_rmq_con(pool: Pool) -> Result<models::Connection, deadpool_lapin::PoolError> {
+    let connection = pool.get().await?;
+    Ok(connection)
+}
+
+async fn add_msg_handler(
+    pool: Pool,
+    payload: &models::ClaimMintRewards,
+    rate_limiter: &mut DirectRateLimiter<LeakyBucket>,
+) -> Result<String, Rejection> {
+    match rate_limiter.check() {
+        Ok(_) => (),
+        Err(_) => return Err(error::Error::RateLimitReachedError.into()),
+    }
+
+    let payload = serde_json::json!(payload).to_string();
+
+    let rmq_con = get_rmq_con(pool.clone()).await.map_err(|e| {
+        eprintln!("can't connect to rmq, {}", e);
+        warp::reject::custom(error::Error::RMQPoolError(e))
+    })?;
+
+    let channel = rmq_con.create_channel().await.map_err(|e| {
+        eprintln!("can't create channel, {}", e);
+        warp::reject::custom(error::Error::RMQError(e))
+    })?;
+
+    let q = channel
+        .queue_declare(
+            "mint_response",
+            lapin::options::QueueDeclareOptions::default(),
+            lapin::types::FieldTable::default(),
+        )
+        .await
+        .unwrap();
+    log::debug!("Quelength: {}", q.message_count());
+
+    channel
+        .basic_publish(
+            "",
+            "mint_response",
+            lapin::options::BasicPublishOptions::default(),
+            payload.as_bytes(),
+            lapin::BasicProperties::default(),
+        )
+        .await
+        .map_err(|e| {
+            eprintln!("can't publish: {}", e);
+            warp::reject::custom(error::Error::RMQError(e))
+        })?
+        .await
+        .map_err(|e| {
+            eprintln!("can't publish: {}", e);
+            warp::reject::custom(error::Error::RMQError(e))
+        })?;
+    Ok(serde_json::json!({"status":"successfull","spot":q.message_count()}).to_string())
+}
---
+++ worker/work_loki/src/main.rs
@@ -0,0 +1,160 @@
+/*
+#################################################################################
+# See LICENSE.md for full license information.                                  #
+# Software: Drasil Blockchain Application Framework                             #
+# License: Drasil Source Available License v1.0                                 #
+# Licensors: Torben Poguntke (torben@drasil.io) & Zak Bassey (zak@drasil.io)    #
+#################################################################################
+*/
+extern crate pretty_env_logger;
+
+mod error;
+mod handlers;
+mod models;
+
+use deadpool_lapin::Pool;
+use lapin::ConnectionProperties;
+use lazy_static::lazy_static;
+use std::env;
+
+lazy_static! {
+    static ref AMQP_ADDR: String =
+        std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://rmq:rmq@127.0.0.1:5672/%2f".into());
+    static ref QUEUE_NAME: String =
+        std::env::var("QUEUE_NAME").unwrap_or_else(|_| "mint_response".to_string());
+    static ref CONSUMER_NAME: String =
+        std::env::var("CONSUMER_NAME").unwrap_or_else(|_| "work_loki_0".to_string());
+}
+
+#[tokio::main]
+async fn main() -> Result<(), error::Error> {
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "info");
+    }
+    pretty_env_logger::init();
+    // RMQ
+    let manager =
+        deadpool_lapin::Manager::new(AMQP_ADDR.to_string(), ConnectionProperties::default());
+    let pool: deadpool_lapin::Pool = deadpool::managed::Pool::builder(manager)
+        .max_size(100)
+        .build()
+        .expect("can't create pool");
+    log::debug!("pool: {:?}", pool);
+
+    let _ = futures::join!(rmq_listen(pool));
+    Ok(())
+}
+
+async fn get_rmq_con(pool: Pool) -> Result<models::Connection, deadpool_lapin::PoolError> {
+    let connection = pool.get().await?;
+    Ok(connection)
+}
+
+async fn rmq_listen(pool: deadpool_lapin::Pool) -> Result<(), error::Error> {
+    let mut retry_interval = tokio::time::interval(tokio::time::Duration::from_secs(5));
+    loop {
+        retry_interval.tick().await;
+        log::debug!("connecting rmq consumer...");
+        match init_rmq_listen(pool.clone()).await {
+            Ok(_) => log::debug!("rmq listen returned"),
+            Err(e) => log::debug!("rmq listen had an error: {}", e),
+        };
+    }
+}
+
+async fn init_rmq_listen(pool: Pool) -> Result<(), error::Error> {
+    let rmq_con = get_rmq_con(pool).await.map_err(|e| {
+        log::error!("could not get rmq con: {}", e);
+        e
+    })?;
+    let channel = rmq_con.create_channel().await?;
+
+    let queue = channel
+        .queue_declare(
+            &QUEUE_NAME.to_string(),
+            lapin::options::QueueDeclareOptions::default(),
+            lapin::types::FieldTable::default(),
+        )
+        .await?;
+    log::debug!("Declared queue {:?}", queue);
+
+    let mut consumer = channel
+        .basic_consume(
+            &QUEUE_NAME.to_string(),
+            &CONSUMER_NAME.to_string(),
+            lapin::options::BasicConsumeOptions::default(),
+            lapin::types::FieldTable::default(),
+        )
+        .await?;
+
+    log::debug!("rmq consumer connected, waiting for messages");
+    while let Some(delivery) = tokio_stream::StreamExt::next(&mut consumer).await {
+        if let Ok(deliv) = delivery {
+            log::debug!("consumer received msg: {:?}", deliv);
+            // Do something with message
+            let data: models::ClaimMintRewards =
+                serde_json::from_str(std::str::from_utf8(&deliv.data)?)?;
+            log::debug!("Data {:?}", data);
+            // Acknowledge message if successfull
+            log::debug!("try to esteblish connection ...");
+            let gcon = &mut gungnir::establish_connection()?;
+            log::debug!("try to get mint project ...");
+            let mp =
+                match gungnir::minting::models::MintProject::get_mintproject_by_id(gcon, data.mpid)
+                {
+                    Ok(o) => o,
+                    Err(e) => {
+                        log::error!("could not find mint project: {}", e.to_string());
+                        channel
+                            .basic_reject(
+                                deliv.delivery_tag,
+                                lapin::options::BasicRejectOptions::default(),
+                            )
+                            .await?;
+                        return Err(crate::error::Error::Custom(
+                            "could not find mint project:".to_owned(),
+                        ));
+                    }
+                };
+            log::debug!("try to claim nft ...");
+            let nft = match gungnir::minting::models::Nft::claim_random_unminted_nft(
+                mp.id,
+                &mp.nft_table_name,
+                &data.claim_addr,
+                0,
+            )
+            .await
+            {
+                Ok(o) => o,
+                Err(_) => {
+                    channel
+                        .basic_reject(
+                            deliv.delivery_tag,
+                            lapin::options::BasicRejectOptions::default(),
+                        )
+                        .await?;
+                    None
+                }
+            };
+
+            match nft {
+                Some(n) => {
+                    log::debug!("nft: {:?}", n);
+                }
+                None => {
+                    return Err(crate::error::Error::Custom(
+                        "Could not reserve NFT".to_owned(),
+                    ));
+                }
+            }
+
+            channel
+                .basic_ack(
+                    deliv.delivery_tag,
+                    lapin::options::BasicAckOptions::default(),
+                )
+                .await?
+        }
+    }
+    Ok(())
+}
