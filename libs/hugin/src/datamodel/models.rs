0fb82b2c038348e55e96786b6669e27da65826b3
Amir Abdin <amir.abdin@worldmobile.io>
2023-08-08 16:06:07 +0200

    Apply feedback

--- libs/hugin/src/datamodel/models.rs
+++ libs/hugin/src/datamodel/models.rs
@@ -648,6 +648,7 @@ impl Operation {
                 for t in transfers {
                     let receiver = b_decode_addr_na(&t.receiving_address).unwrap();
                     let mut assets = Vec::<StdAssetHandle>::new();
+                    let metadata = None;
                     for n in &t.asset_handles {
                         let policy = if let Some(p) = &n.policy {
                             Some(PolicyID::from_hex(p)?)
@@ -668,7 +669,7 @@ impl Operation {
                             metadata: n.metadata.as_ref().cloned(),
                         })
                     }
-                    trans.push(AssetTransfer { receiver, assets })
+                    trans.push(AssetTransfer { receiver, assets, metadata })
                 }
                 let wal_addr = if let Some(addr) = wallet_addresses {
                     let r = addr.iter().fold(Vec::<Address>::new(), |mut acc, a| {
