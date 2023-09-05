5058f58cd427341f1ba017d24e1100cd3a1cf069
Styvane Soukossi <4968379+styvane@users.noreply.github.com>
2023-09-05 07:26:19 +0200

    Add integration tests setup and test contract listing api

---
+++ services/heimdallr-v2/tests/api/testapp.rs
@@ -0,0 +1,37 @@
+use heimdallr_v2::bootstrap::Application;
+use heimdallr_v2::settings::Settings;
+
+/// Test application
+pub struct TestApp {
+    pub client: reqwest::Client,
+    pub address: String,
+}
+
+impl TestApp {
+    /// Spawns new test application.
+    pub async fn spawn() -> Self {
+        let client = reqwest::Client::builder()
+            .build()
+            .expect("failed to create client");
+
+        let settings = {
+            let mut settings = Settings::load().expect("failed to load settings");
+            settings.application.port = 0;
+            settings
+        };
+
+        let host = settings.application.host.clone();
+        let app = Application::new(settings)
+            .await
+            .expect("failed to create application");
+
+        let address = app
+            .listener
+            .local_addr()
+            .map(|addr| format!("http://{host}:{}", addr.port()))
+            .expect("failed to get local address");
+
+        let _ = tokio::spawn(app.listen_and_serve());
+        Self { client, address }
+    }
+}
