edb226f32599ccb409bee361e1b8ce3c3f85eb4f
Torben Poguntke <torben.poguntke@worldmobile.io>
2023-10-03 13:14:01 +0200

    fix-vidar

--- services/loki/src/filters.rs
+++ services/loki/src/filters.rs
@@ -20,7 +20,7 @@ pub fn resp_option() -> impl Filter<Extract = impl warp::Reply, Error = warp::Re
     warp::options()
         .and(warp::header("origin"))
         .map(|origin: String| {
-            Ok(warp::http::Response::builder()
+            warp::http::Response::builder()
                 .status(warp::http::StatusCode::OK)
                 .header("access-control-allow-methods", "HEAD, GET, POST, OPTION")
                 .header("access-control-allow-headers", "authorization")
@@ -28,7 +28,7 @@ pub fn resp_option() -> impl Filter<Extract = impl warp::Reply, Error = warp::Re
                 .header("access-control-max-age", "300")
                 .header("access-control-allow-origin", origin)
                 .header("vary", "origin")
-                .body(""))
+                .body("")
         })
 }
 
