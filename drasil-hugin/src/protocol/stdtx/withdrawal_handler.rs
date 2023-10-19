e2801a38202bb3e88b0e8a33bf20d0549196e242
Amir Abdin <amir.abdin@worldmobile.io>
2023-10-19 13:32:06 +0200

    Apply feedback

--- drasil-hugin/src/protocol/stdtx/withdrawal_handler.rs
+++ drasil-hugin/src/protocol/stdtx/withdrawal_handler.rs
@@ -29,42 +29,41 @@ pub(crate) async fn handle_reward_withdrawal(bst: &BuildStdTx) -> crate::Result<
         )?
     )? as u64);
 
-    let bld_tx = &drasil_murin::TxBuilder::new(
-            &gtxd,
-            &Vec::<String>::new()
-        )
-        .build(
-            &drasil_murin::txbuilder::stdtx::AtAWBuilder::new(
-                &match op {
-                    Operation::RewardWithdrawal{} => op.into_withdrawal().await?,
-                    _ => {
-                        return Err(CmdError::Custom {
-                            str: format!("ERROR wrong input data provided for '{:?}'", bst.tx_type()),
-                        }
-                        .into())
-                    }
-                }
-            )
-        )
-        .await?;
+    let tx_data = &drasil_murin::TxBuilder::new(&gtxd, &Vec::<String>::new());
 
-    Ok(
-        create_response(
-            &bld_tx,
-            &RawTx::new(
-                &bld_tx.get_tx_body(), 
-                &bld_tx.get_txwitness(), 
-                &bld_tx.get_tx_unsigned(), 
-                &bld_tx.get_metadata(), 
-                &gtxd.to_string(), 
-                &"".to_string(), 
-                &bld_tx.get_used_utxos(), 
-                &hex::encode(gtxd.get_stake_address().to_bytes()), 
-                &bst.customer_id(), 
-                &[-1]
-            ),
-            bst.transaction_pattern().wallet_type().as_ref(),
-        )?
-        .to_string()
-    )
+    let app_type = &drasil_murin::txbuilder::stdtx::AtAWBuilder::new(
+        &match op {
+            Operation::RewardWithdrawal{} => op.into_withdrawal().await?,
+            _ => {
+                return Err(CmdError::Custom {
+                    str: format!("ERROR wrong input data provided for '{:?}'", bst.tx_type()),
+                }
+                .into())
+            }
+        }
+    );
+
+    let bld_tx = tx_data.build(app_type).await?;
+
+    let raw_tx = &RawTx::new(
+        &bld_tx.get_tx_body(), 
+        &bld_tx.get_txwitness(), 
+        &bld_tx.get_tx_unsigned(), 
+        &bld_tx.get_metadata(), 
+        &gtxd.to_string(), 
+        &"".to_string(), 
+        &bld_tx.get_used_utxos(), 
+        &hex::encode(gtxd.get_stake_address().to_bytes()), 
+        &bst.customer_id(), 
+        &[-1]
+    );
+
+    let response = create_response(
+        &bld_tx,
+        raw_tx,
+        bst.transaction_pattern().wallet_type().as_ref(),
+    )?
+    .to_string();
+
+    Ok(response)
 }
