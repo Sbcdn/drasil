1208e6c18923865f5ff9125f2d178fa5bbe9f02c
Torben Poguntke <torben.poguntke@worldmobile.io>
2023-11-17 00:49:23 +0100

    local

--- drasil-dvltath/src/vault/auth.rs
+++ drasil-dvltath/src/vault/auth.rs
@@ -1,4 +1,5 @@
 use lazy_static::lazy_static;
+use log::info;
 use std::env::{set_var, var};
 use std::io::{Read, Write};
 use vaultrs::api::auth::approle::responses::GenerateNewSecretIDResponse;
@@ -128,25 +129,27 @@ async fn request_secret(
 
     let mut response = client.get(url);
 
-    match tokio::time::timeout(std::time::Duration::from_secs(1), &mut response).await {
-        Err(_) => {
-            log::error!("secret request timeout");
+    match tokio::time::timeout(std::time::Duration::from_secs(3), &mut response).await {
+        Err(e) => {
+            log::error!("secret request timeout: {:?}", e);
         }
-        Ok(no_timeout) => match no_timeout {
-            Ok(resp) => {
-                log::debug!("Response: {:?}", resp);
-                let r_status = resp.status();
-                //let resp_text = resp.text().await.unwrap();
-                if r_status != http::StatusCode::ACCEPTED {
-                    log::error!("secret request not accepted");
-                } else {
-                    return Ok(read_wrapped_secret(vclient, path).await);
+        Ok(no_timeout) => {
+            info!("no timeout: {:?}", no_timeout); 
+            match no_timeout {   
+                Ok(resp) => {
+                    log::debug!("Response: {:?}", resp);
+                    let r_status = resp.status();
+                    //let resp_text = resp.text().await.unwrap();
+                    if r_status != http::StatusCode::ACCEPTED {
+                        log::error!("secret request not accepted");
+                    } else {
+                        return Ok(read_wrapped_secret(vclient, path).await);
+                    }
                 }
-            }
-            Err(_) => {
-                log::error!("error on secret request");
-            }
-        },
+                Err(e) => {
+                    log::error!("error on secret request {:?}",e);
+                }
+            }}
     }
     Err(crate::error::Error::StdError)
 }
--- local/scripts/setup_vault.sh
+++ local/scripts/setup_vault.sh
@@ -4,3 +4,11 @@ kubectl cp ./local/scripts/configure.sh default/vault-0:/tmp/ -c vault; \
 kubectl get pods vault-0 -ojson | jq '.status.containerStatuses[].ready'; \
 kubectl exec -i "vault-0" -- chmod +x /tmp/configure.sh; \
 kubectl exec -i "vault-0" -- /tmp/configure.sh
+APP_ROLE_ID=$(kubectl exec -i vault-0 -- vault read -format=json auth/approle/role/drslapp/role-id | jq '.data.role_id')
+kubectl create secret generic app-vault \
+    --from-literal=role-id=${APP_ROLE_ID} 
+DVLTATH_ROLE_ID=$(kubectl exec -i vault-0 -- vault read -format=json auth/approle/role/mngdrslapp/role-id | jq -r '.data.role_id')
+DVLTATH_SECRET_ID=$(kubectl exec -i vault-0 -- vault write -f -format=json auth/approle/role/mngdrslapp/secret-id | jq -r '.data.secret_id')
+kubectl create secret generic dvltath-vault \
+    --from-literal=role-id=${DVLTATH_ROLE_ID} \
+    --from-literal=secret-id=${DVLTATH_SECRET_ID} 
